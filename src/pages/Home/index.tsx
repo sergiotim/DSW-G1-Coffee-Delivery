a7730d1ce711f9c9983242fe9d0e3987c05dabaf
Lucas Pedro <lucaspedrolbg@gmail.com>
2025-04-03 23:02:03 -0300

    feat: commit initial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,116 @@
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+import { useTheme } from 'styled-components'
+
+import { Card } from '../../components/Card'
+
+import { CoffeeList, Heading, Hero, HeroContent, Info } from './styles'
+import { useEffect, useState } from 'react'
+import {api} from '../../serves/api';
+
+interface Coffee {
+  id: string;
+  title: string;
+  description: string;
+  tags: string[];
+  price: number;
+  image: string;
+};
+
+interface CoffeeResponse {
+  coffees: Coffee[];
+}
+
+export function Home() {
+  const theme = useTheme();
+  const [coffees, setCoffees] = useState<Coffee[]>([]);
+
+  useEffect(() => {
+    async function fetchCoffees() {
+      try {
+        const response = await api.get<Coffee[]>('/coffees');
+        console.log(response);
+
+        setCoffees(response.data);
+      } catch (error) {
+        console.error('Error fetching coffees:', error);
+      }
+    }
+
+    fetchCoffees();
+  }, []);
+
+  return (
+    <div>
+      <Hero>
+        <HeroContent>
+          <div>
+            <Heading>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+
+              <span>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </span>
+            </Heading>
+
+            <Info>
+              <div>
+                <ShoppingCart
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                />
+                <span>Compra simples e segura</span>
+              </div>
+
+              <div>
+                <Package
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['base-text'] }}
+                />
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+
+              <div>
+                <Timer
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.yellow }}
+                />
+                <span>Entrega rápida e rastreada</span>
+              </div>
+
+              <div>
+                <Coffee
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.purple }}
+                />
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </Info>
+          </div>
+
+          <img src="/images/hero.svg" alt="Café do Coffee Delivery" />
+        </HeroContent>
+
+        <img src="/images/hero-bg.svg" id="hero-bg" alt="" />
+      </Hero>
+
+      <CoffeeList>
+        <h2>Nossos cafés</h2>
+
+        <div>
+          {coffees.map((coffee) => (
+            <Card key={coffee.id} coffee={coffee} />
+          ))}
+        </div>
+      </CoffeeList>
+    </div>
+  )
+}
